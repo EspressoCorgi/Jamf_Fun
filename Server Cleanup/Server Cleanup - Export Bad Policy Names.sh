30bce189cc4455f3378f7e4a5bce128322ef7ed0
espressocorgi <shem@tamu.edu>
2023-10-11 10:57:20 -0500

    more extention attributes & server clean up tools

--- Server Cleanup/Server Cleanup - Export Bad Policy Names.sh
+++ Server Cleanup/Server Cleanup - Export Bad Policy Names.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Get the current logged-in user's home directory
+user_home_dir=$(eval echo "~$(whoami)")
+
 # Jamf Pro server URL
-jamfProURL="your_server_url_here"
+jamfProURL="https://server.jamfcloud.com"
 
 # Jamf API credentials - Consider using environment variables for these
-jamfusername="your_username_here"
-jamfpassword="your_password_here"
+jamfusername="username"
+jamfpassword="password"
 
 # Define the CSV file name with the desired format
 currentDate=$(date "+%Y-%m-%d")
@@ -14,21 +17,38 @@ csvFileName="bad_policy_names_$currentDate.csv"
 # Define the output directory as a temporary directory
 OUTPUT_DIR=$(mktemp -d)
 
+# Function to sanitize policy names
+sanitize_policy_name() {
+    local policy_name="$1"
+    # Remove HTML tags and special characters from the policy name
+    sanitized_name=$(echo "$policy_name" | sed 's/<[^>]*>//g' | tr -d '[:punct:]')
+    echo "$sanitized_name"
+}
+
 # Function to export a policy
 export_policy() {
     local policy_id="$1"
     local policy_name="$2"
-    local output_dir="$3"
 
-    # Export the policy
-    if curl -o "$output_dir/$policy_name.xml" -u "$jamfusername:$jamfpassword" "$jamfProURL/JSSResource/policies/id/$policy_id" >/dev/null 2>&1; then
-        echo "Exported policy: $policy_name (ID: $policy_id)"
+    # Sanitize the policy name
+    sanitized_policy_name=$(sanitize_policy_name "$policy_name")
+
+    # Export the policy to a temporary XML file
+    local temp_xml=$(mktemp)
+    if curl -o "$temp_xml" -u "$jamfusername:$jamfpassword" "$jamfProURL/JSSResource/policies/id/$policy_id" >/dev/null 2>&1; then
+        echo "Exported policy: $sanitized_policy_name (ID: $policy_id)"
+        # Extract the site ID from the policy XML
+        site_id=$(xmlstarlet sel -t -v "//site/id" "$temp_xml")
+        # Fetch the site name based on the site ID
+        site_name=$(curl -s -u "$jamfusername:$jamfpassword" "$jamfProURL/JSSResource/sites/id/$site_id" | xmlstarlet sel -t -v "//site/name")
+        # Append sanitized policy name, ID, and site name to the CSV file in the user's "Downloads" folder
+        echo "$sanitized_policy_name,$policy_id,$site_name" >> "$user_home_dir/Downloads/$csvFileName"
     else
-        echo "Failed to export policy: $policy_name (ID: $policy_id)"
+        echo "Failed to export policy: $sanitized_policy_name (ID: $policy_id)"
     fi
 
-    # Append policy name and ID to the CSV file
-    echo "$policy_name,$policy_id" >> "$csvFilePath"
+    # Remove the temporary XML file
+    rm -f "$temp_xml"
 }
 
 # Trap the script exit to ensure cleanup
@@ -40,11 +60,8 @@ trap cleanup_temp_directory EXIT
 # Get the list of all policies from Jamf Pro in XML format
 policies_xml=$(curl -s -u "$jamfusername:$jamfpassword" "$jamfProURL/JSSResource/policies")
 
-# Define the CSV file path
-csvFilePath="$OUTPUT_DIR/$csvFileName"
-
-# Write the CSV file header
-echo "Policy Name,Policy ID" > "$csvFilePath"
+# Write the CSV file header in the user's "Downloads" folder
+echo "Policy Name,Policy ID,Site Name" > "$user_home_dir/Downloads/$csvFileName"
 
 # Define the abbreviations to filter policies
 abbreviations=("Global" "AS" "ATH" "CD" "CAS" "ENG" "TAMUG" "TAMUH" "ITR" "ITSAP" "LCR" "RL" "PVFA" "SVM" "TS")
@@ -72,12 +89,13 @@ while IFS= read -r line; do
 
     # Export the policy if it doesn't match any abbreviation
     if [ "$matched" = false ]; then
-        export_policy "$policy_id" "$policy_name" "$OUTPUT_DIR"
+        export_policy "$policy_id" "$policy_name"
         ((exportedPolicyCount++))
     fi
 done < <(echo "$policies_xml" | xmlstarlet sel -t -m "//policy" -v "id" -o $'\t' -v "name" -n)
 
-# Update the CSV file with the total number of exported policies
-echo "Total Policies,$exportedPolicyCount" >> "$csvFilePath"
+# Update the CSV file in the user's "Downloads" folder with the total number of exported policies
+echo "Total Policies,$exportedPolicyCount" >> "$user_home_dir/Downloads/$csvFileName"
 
-echo "Exported policies that do not meet the criteria to $OUTPUT_DIR"
+echo "Exported policies that do not meet the criteria to $user_home_dir/Downloads/$csvFileName"
+This updated script includes a sanitize_policy_name function that removes HTML tags and special characters from policy names before exporting them to the CSV file. This should help prevent parsing errors caused by unexpected HTML content in policy names.
